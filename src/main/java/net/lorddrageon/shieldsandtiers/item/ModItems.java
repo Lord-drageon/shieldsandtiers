14f93b8966fc1d9c09ad2241a9f4241ed2bcbb12
Thomas Mueller <tommotabq@gmail.com>
2023-04-05 16:10:28 -0600

    netherite shield was marked as diamond shield

--- src/main/java/net/lorddrageon/shieldsandtiers/item/ModItems.java
+++ src/main/java/net/lorddrageon/shieldsandtiers/item/ModItems.java
@@ -5,6 +5,7 @@ import net.lorddrageon.shieldsandtiers.ShieldsAndTiers;
 import net.lorddrageon.shieldsandtiers.item.custom.DiamondShield;
 import net.lorddrageon.shieldsandtiers.item.custom.IronShield;
 import net.lorddrageon.shieldsandtiers.item.custom.LeatherShield;
+import net.lorddrageon.shieldsandtiers.item.custom.NetheriteShield;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -24,7 +25,7 @@ public class ModItems {
     public static Item DSHIELD=Registry.register(Registry.ITEM, new Identifier(ShieldsAndTiers.MOD_ID, "diamond_shield"),
             DIAMOND_SHIELD);
 
-    public static Item NETHERITE_SHIELD=new DiamondShield(new FabricItemSettings().maxDamage(2500).group(ItemGroup.COMBAT));
+    public static Item NETHERITE_SHIELD=new NetheriteShield(new FabricItemSettings().maxDamage(2500).group(ItemGroup.COMBAT));
     public static Item NSHIELD=Registry.register(Registry.ITEM, new Identifier(ShieldsAndTiers.MOD_ID, "netherite_shield"),
             NETHERITE_SHIELD);
     public static void registerModItems(){
